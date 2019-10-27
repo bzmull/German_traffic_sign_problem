17afcf61ee8fb75ee9e7925cacce4f8a8f5de39c
bzmull <bmuller94@gmail.com>
2019-10-27 17:27:45 -0400

    revert to default preprocessing

--- main.py
+++ main.py
@@ -97,8 +97,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import numpy as np
-import matplotlib.pyplot as plt
+# import numpy as np
+# import matplotlib.pyplot as plt
 
 
 from torchvision import datasets, transforms, utils
