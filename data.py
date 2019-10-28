c2b25baf47abb9f545c4421efab2b4b1af2dea88
bzmull <bmuller94@gmail.com>
2019-10-28 07:28:15 -0400

    grayscale transform applied

--- data.py
+++ data.py
@@ -9,9 +9,11 @@ import torchvision.transforms as transforms
 # and normalize them to mean = 0 and standard-deviation = 1 based on statistics collected from
 # the training set
 data_transforms = transforms.Compose([
+    transforms.Grayscale(num_output_channels=1),  # applied grayscale to images # SWITCH TO GRAYSCALE
     transforms.Resize((32, 32)),
     transforms.ToTensor(),
-    transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
+    # transforms.Normalize((0.3337, 0.3064, 0.3171), ( 0.2672, 0.2564, 0.2629))
+    transforms.Normalize((0.3337,), (0.2672,))  # images are grayscaled which means one channel ==> normalize only one dimension  # SWITCH TO GRAYSCALE
 ])
 train_data_transform = transforms.Compose([
     transforms.Grayscale(num_output_channels=1),  # applied grayscale to images # SWITCH TO GRAYSCALE
