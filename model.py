2c5f8e252c681d7e313df660de5176b2ac8a194d
bzmull <bmuller94@gmail.com>
2019-10-28 23:45:55 -0400

    edited original architecture a little

--- model.py
+++ model.py
@@ -41,8 +41,10 @@ class Net(nn.Module):
 # ---------- ORIGINAL MODEL ------------------------------------------------------------------------------------------ 
         self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv3 = nn.Conv2d(20, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(500, 50)
+        # self.fc1 = nn.Linear(500, 50)
+        self.fc1 = nn.Linear(20, 50)
         self.fc2 = nn.Linear(50, nclasses)
 # ----------------------------------------------------------------------------------------------------
         
@@ -80,12 +82,15 @@ class Net(nn.Module):
 # ---------- ORIGINAL MODEL ------------------------------------------------------------------------------------------
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 500)
+        # print(x.shape)
+        x = F.relu(self.conv3(x))
+        # print(x.shape)
+        # x = x.view(-1, 500)
+        x = x.view(-1, 20)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
         return F.log_softmax(x)
-        print(x.shape)
 # ----------------------------------------------------------------------------------------------------
 # ---------- FIRST MODEL ------------------------------------------------------------------------------------------        
 #         x = self.activation(self.conv1(x))
